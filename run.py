9219c90069527caac5fede01db9fc60c409dc9d4
eden essame <eden_essame80@hotmail.com>
2024-04-15 14:42:09 +0000

    outline the main functions

--- run.py
+++ run.py
@@ -1,3 +1,47 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def get_name():
+    """
+    Asks user for their name to display
+    """
+    pass
+
+def generate_word():
+    """
+    Randomly select a word to use from a list 
+    """
+    pass
+
+def hide_word():
+    """
+    Hide the word so it can't be seen
+    """
+    pass
+
+def is_input_valid():
+    """
+    Check data input is valid
+    Only allow letters and strings 
+    Don't allow already guessed letters and string
+    """
+    pass
+
+def play_game():
+    """
+    Allow the user to make guesses
+    Keep track of guessed letters and words so they can't be guessed again
+    Keep track of lives used
+    """
+    pass 
+
+def display_hangman():
+    """
+    Create the hangman display in its different stages
+    """
+    pass
+
+def main():
+    """
+    calls all the main functions
+    """
+    pass
