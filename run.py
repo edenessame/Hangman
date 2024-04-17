950a8917359cf0a71e41a3ccaabdc8986149135b
eden essame <eden_essame80@hotmail.com>
2024-04-17 15:48:39 +0000

    add while loop to play game function

--- run.py
+++ run.py
@@ -31,7 +31,35 @@ def play_game():
     Keep track of guessed letters and words so they can't be guessed again
     Keep track of lives used
     """
-    pass 
+    guessed_letters = []
+    guessed_words = []
+    lives = 6
+    game_over = False
+
+    while not game_over and lives > 0:
+        guess = input("Please guess a letter or word: ")
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print(f"You already guessed: {guess}!")
+            elif guess not in word:
+                print(f"Sorry! {guess} is not in the word.") 
+                lives -= 1
+                guessed_letters.append(guess)   
+            else:
+                print(f"Well done! {guess} is in the word!")    
+                guessed_letters.append(guess)
+        elif len(guess) == len(word) and guess.isalpha(): 
+            if guess in guessed_words:
+                print(f"You already guessed: {guess}!")
+            elif guess != word:
+                print(f"Sorry! {guess} is not the word!") 
+                lives -= 1
+                guessed_words.append(guess)   
+            else:
+                guessed = True 
+        else:
+            print("Not a valid Guess, please try again")                     
+
 
 def display_hangman():
     """
@@ -47,3 +75,4 @@ def main():
 
 get_name()
 generate_word()
+play_game()
